54ca574b4fd777f550540a5c67bb06eb61dbc8a5
Thomas David <thomasdavid0@gmail.com>
2021-11-02 11:01:03 +0000

    update templates

--- flightplotting/templates.py
+++ flightplotting/templates.py
@@ -6,9 +6,9 @@ pio.templates["flight3d"] = go.layout.Template(layout=go.Layout(
     margin=dict(l=0, r=0, t=0, b=0),
     scene=dict(
         aspectmode='data',
-        xaxis=dict(visible=False, showticklabels=False),
-        yaxis=dict(visible=False, showticklabels=False),
-        zaxis=dict(visible=False, showticklabels=False),
+        #xaxis=dict(visible=True, showticklabels=True),
+        #yaxis=dict(visible=True, showticklabels=True),
+        #zaxis=dict(visible=True, showticklabels=True),
     ),
     legend=dict(
         font=dict(size=20),
@@ -23,6 +23,6 @@ pio.templates["flight3d"] = go.layout.Template(layout=go.Layout(
 pio.templates["judge_view"] = go.layout.Template(layout=go.Layout(scene_camera=dict(
     up=dict(x=0, y=0, z=1),
     center=dict(x=0, y=0, z=-0.2),
-    eye=dict(x=0.0, y=-1.6, z=-0.8),
+    eye=dict(x=0.0, y=-2.3, z=-0.8),
     projection=dict(type='perspective')
 )))
