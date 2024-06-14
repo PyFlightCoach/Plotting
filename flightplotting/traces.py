4d3a402af250c70901fe6eb7b1f143837e1ae4f6
Thomas David <thomasdavid0@gmail.com>
2024-06-14 16:00:57 +0100

    bugfix axis rate trace

--- flightplotting/traces.py
+++ flightplotting/traces.py
@@ -154,11 +154,12 @@ def axis_rate_traces(sts: dict[str, State], cols='pqr'):
     cols = px.colors.qualitative.D3
     traces = []
     dashes = ['solid', 'dot', 'dash', 'longdash', 'dashdot', 'longdashdot']
+    st0 = list(sts.values())[0]
     for i, rv in enumerate(list('pqr')):
         for k, st in sts.items():
 
             traces.append(go.Scatter(
-                x=st.data.index, y=getattr(st, rv), name=f'{k} {rv}', line=dict(color=cols[i], dash=dashes[list(sts.keys()).index(k)])
+                x=st0.data.index - st0.data.index[0], y=getattr(st, rv), name=f'{k} {rv}', line=dict(color=cols[i], dash=dashes[list(sts.keys()).index(k)])
             ))
     return traces
 
